8da3129fccb1ab537981914e4078ca7c8912a894
chengye1 <chengye1@xiaomi.com>
2020-02-15 02:46:54 +0800

    get

--- src/controllers/entry.go
+++ src/controllers/entry.go
@@ -45,7 +45,7 @@ func registerMiddleware(router *gin.Engine)  {
 
 func registerHandler(router *gin.Engine)  {
 	//注册接口
-	router.GET("/welcome", GetHandler)
+	router.GET("/dev/welcome", GetHandler)
 	router.POST("/api/home/products", ProductHandler)
 	router.PUT("/server/put", PutHandler)
 	router.DELETE("/server/delete", DeleteHandler)
